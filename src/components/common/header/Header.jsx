1e4807d20d164c2b03e7ec4072a76257a10b62a5
samueltilahun <samueltilahun991@gmail.com>
2024-05-15 10:20:43 +0300

    some updates

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import logo from "../../../components/data/images/logo.png"
+import { Link } from 'react-router-dom'
 import "./header.css"
+import {nav} from "../../data/Data"
+import { NavLink} from 'react-router-dom/cjs/react-router-dom.min'
 
 const Header = () => {
   return (
@@ -9,33 +12,40 @@ const Header = () => {
             <div className="logoDiv">
                 <img className='logo' src={logo} alt="logo" />
             </div>
-            <div className="navBar flex">
-                <ul className="navLists flex">
+            <div className="nav">  
+                {
+                    nav.map((list, index) => ( 
+                        <NavLink key={index} to={list.path}>{list.text}</NavLink>
+                    ))
+                }
+            </div>
+            {/* <div className="navBar flex">
+                <ul className="navLists">
                     <li className="navItem">
                         <a href="home" className='navLink'>Home</a>
-                    </li>
+                    </li> 
                 </ul>
-                <ul className="navLists flex">
+                <ul className="navLists">
                     <li className="navItem">
                         <a href="home" className='navLink'>About</a>
                     </li>
                 </ul>
-                <ul className="navLists flex">
+                <ul className="navLists">
                     <li className="navItem">
                         <a href="home" className='navLink'>Services</a>
                     </li>
                 </ul>
-                <ul className="navLists flex">
+                <ul className="navLists">
                     <li className="navItem">
                         <a href="home" className='navLink'>Pricing</a>
                     </li>
                 </ul>
-                <ul className="navLists flex">
+                <ul className="navLists">
                     <li className="navItem">
                         <a href="home" className='navLink'>Contact</a>
                     </li>
                 </ul>
-            </div>
+            </div> */}
         </div>
     </header>
   )
---
+++ src/components/data/Data.jsx
@@ -0,0 +1,19 @@
+export const nav = [
+    {
+        text: "home",
+        path: "/"
+    },
+    {
+        text: "about",
+        path: "/about"
+    },
+    {
+        text: "services",
+        path: "/services"
+    },
+    {
+        text: "contact",
+        path: "/contact"
+    }
+]
+
